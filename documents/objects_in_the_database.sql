0a9468e324c41c6b8a5c7914031b4cc2cb6b3d1c
Amanuel Gebreegziabhare <amanuelgebreegziabhare@gmail.com>
2026-09-01 13:06:48 +0400

    Add SQL queries to explore database columns
    Added queries to explore columns in the database.

--- documents/objects_in_the_database.sql
+++ documents/objects_in_the_database.sql
@@ -1,3 +1,12 @@
 --Explore all Objects in the Database 
 SELECT * 
 FROM INFORMATION_SCHEMA.TABLES  
+
+-- Explore all Columns in the Database 
+SELECT * 
+FROM INFORMATION_SCHEMA.COLUMNS
+
+-- Explore all Columns of a specific table in the Database 
+SELECT * 
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_NAME = 'dim_customers' 
