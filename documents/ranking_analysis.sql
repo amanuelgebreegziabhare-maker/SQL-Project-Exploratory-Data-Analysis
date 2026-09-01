1e5fd75e2862c99673ee0bba139d8ef150013808
Amanuel Gebreegziabhare <amanuelgebreegziabhare@gmail.com>
2026-09-01 15:52:00 +0400

    Add query for top 5 revenue-generating products
    Added SQL query to retrieve top 5 products by revenue.

--- documents/ranking_analysis.sql
+++ documents/ranking_analysis.sql
@@ -1,1 +1,12 @@
 
+--Which 5 products generate the highest revenue?
+
+SELECT TOP 5
+	p.product_name,
+	SUM(sales_amount) AS total_sales
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+on f.product_key = p.product_key
+WHERE order_number IS NOT NULL
+GROUP BY p.product_name
+ORDER BY SUM(sales_amount)
