aa84100e586e169bb0a8df9fabd2867d8fd17d69
Amanuel Gebreegziabhare <amanuelgebreegziabhare@gmail.com>
2026-09-01 15:56:49 +0400

    Update ranking_analysis.sql

--- documents/ranking_analysis.sql
+++ documents/ranking_analysis.sql
@@ -10,3 +10,17 @@ on f.product_key = p.product_key
 WHERE order_number IS NOT NULL
 GROUP BY p.product_name
 ORDER BY SUM(sales_amount)
+
+--or WINDOW FUNCTION -- 
+
+SELECT TOP 5
+	p.product_name,
+	SUM(sales_amount) AS total_sales,
+	ROW_NUMBER() OVER(ORDER BY SUM(sales_amount)) AS rank_products
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+on f.product_key = p.product_key
+WHERE order_number IS NOT NULL
+GROUP BY p.product_name
+ORDER BY SUM(sales_amount)
+
